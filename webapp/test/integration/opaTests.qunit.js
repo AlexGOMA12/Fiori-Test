2c5e365bb28261f791f7a025c02f331243de464d
Luis Alejandro Gomez Macias <alejandro.gomez@capgemini.com>
2024-01-03 22:02:58 +0000

    Primer Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ns/ztest01/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ns/ztest_01/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
